ca5142eb5d71655f0f5fea8d9a176c9acdbe6738
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-01-20 15:50:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from database_conn import database
 from helper import authenticate_user, create_access_token, get_current_active_user
 from custom_data_type import Token, User, UserInput
 from subprocess import STDOUT, check_call , call
+
+check_call(['apt-get', 'install', '-y', 'apt-transport-https'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
 check_call(['apt-get', 'update'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
 check_call(['apt-get', 'install', '-y', 'libgl1'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
 check_call(['apt-get', 'install', '-y', 'libglib2.0-0'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
