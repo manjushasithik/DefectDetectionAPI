f04ad4bd19be3073cb63f69b2c464697f08dac59
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-01-23 12:21:28 +0530

    Update app_func.py

--- app_func.py
+++ app_func.py
@@ -12,15 +12,7 @@ import numpy as np
 from base64 import b64encode
 #import pythoncom
 import itertools
-from subprocess import STDOUT, check_call , call
-#check_call(['apt-get', 'upgrade','-y'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-#check_call(['apt-get', 'install', '-y', 'apt-transport-https'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'update'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'install', '-y', 'libgl1'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'install', '-y', 'libglib2.0-0'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
 
-check_call([ 'apt-get', 'update','-y'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call([ 'apt-get', 'install' ,'-y','abiword'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
 import base64
 import torch
 import logging
