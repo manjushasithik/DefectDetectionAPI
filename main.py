9d318f7631e662c34818721f5d2adc4e2d69c19c
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-01-23 17:58:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,8 @@ from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from fastapi.staticfiles import StaticFiles
-import os
-import io
-from config import ACCESS_TOKEN_EXPIRE_MINUTES
-from database_conn import database
-from helper import authenticate_user, create_access_token, get_current_active_user
-from custom_data_type import Token, User, UserInput
-from subprocess import STDOUT, check_call , call
-
-#check_call(['apt-get', 'upgrade','-y'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-#check_call(['apt-get', 'install', '-y', 'apt-transport-https'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'update'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'install', '-y', 'libgl1'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call(['apt-get', 'install', '-y', 'libglib2.0-0'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-
-check_call([ 'apt-get', 'update','-y'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-check_call([ 'apt-get', 'install' ,'-y','abiword'], stdout=open(os.devnull,'wb'), stderr=STDOUT)
-#from w3lib.url import parse_data_uri
-
-from app_func import predict
-
-#import torch
-
 app = FastAPI()
 app.mount("/files", StaticFiles(directory="files"), name="files")
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -36,35 +13,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.on_event("startup")
-async def database_connect():
-    await database.connect()
-@app.on_event("shutdown")
-async def database_disconnect():
-    await database.disconnect()
-
-
-# Authentication
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.post("/predict")
-async def fetch_data(userinput: UserInput, current_user: User = Depends(get_current_active_user)):
-    #print(userinput.dict())
-    pred = predict(userinput.dict())
-    print(pred)
-    return pred
+@app.get("test")
+async def test_data():
+    return {"EQUIPMENT_CODE": "ABC",
+            "EQUIPMENT_ID": 23999,
+            "JOB_ID": 3452,
+            "JOB_PLAN_ID": 222,
+            "LOG_ID": 54234,
+            "PDF": "http://localhost:8000/files/report.pdf",
+            "VESSEL_OBJECT_ID": 100000,
+            "cylinder1":{"Fault_id": 99, "Rating": "0","Recommendation": "(Unacceptable) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits\n || (Unacceptable) - Scratch - 1) Adjust the Cylinder Oil Feed rate\n 2) Carry out Drain oil analysis (On board or send ashore)\n 3) Carry out or land samples for Fuel oil analysis\n 4) Check for Hard abrasive particles\n || (Unacceptable) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits\n || (Marginal) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits"},
+            "cylinder2":{"Fault_id": "5", "Rating": "2", "Recommendation": "(Fair) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits"},
+            "cylinder3":{"Fault_id": "4", "Rating": "1", "Recommendation": "(Marginal) - Collapsed - 1) Replace Piston Rings\n 2) Check for Carbon deposits in the ring groove\n 3) Check vertical ring clearance\n 4) Check for Partial sticking\n 5) Check for Poor sealing between the ring and the ring groove floor.\n 6) Check for Clover leafing \n 7) Check for Ring end chamfers.\n 8) Check for too large ring edge radii.\n 9) Check for Continual striking against wear ridges, or other irregularities in the cylinder wall.\n || (Marginal) - Collapsed - 1) Replace Piston Rings\n 2) Check for Carbon deposits in the ring groove\n 3) Check vertical ring clearance\n 4) Check for Partial sticking\n 5) Check for Poor sealing between the ring and the ring groove floor.\n 6) Check for Clover leafing \n 7) Check for Ring end chamfers.\n 8) Check for too large ring edge radii.\n 9) Check for Continual striking against wear ridges, or other irregularities in the cylinder wall.\n || (Fair) - Collapsed - 1) Replace Piston Rings\n 2) Check for Carbon deposits in the ring groove\n 3) Check vertical ring clearance\n 4) Check for Partial sticking\n 5) Check for Poor sealing between the ring and the ring groove floor.\n 6) Check for Clover leafing \n 7) Check for Ring end chamfers.\n 8) Check for too large ring edge radii.\n 9) Check for Continual striking against wear ridges, or other irregularities in the cylinder wall"},
+            "cylinder4":{"Fault_id": 99, "Rating": "0", "Recommendation": "(Unacceptable) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits\n || (Unacceptable) - Carbon - 1) Replace or Overhaul fuel injector to avoid improper combustion\n 2) Adjust fuel temperature/viscocity to attain correct viscocity as per Maker's recommendation\n 3) Check the condition of piston rings free movement (Gas sealing)\n 4) Adjust the Cylinder Oil Feed rate to avoid over lubrication to avoid formation of carbon deposits\n || (Fair) - Too Much Oil - 1) Adjust the Cylinder Oil Feed rate\n 2) Carry out Drain oil analysis (On board or send ashore)\n 3) Adjust feed rate to obtain optimum residual BN"}}
