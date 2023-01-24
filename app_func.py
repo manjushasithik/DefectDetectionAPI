137e53bc7e26666bb689c1af2fab59c4285aa2b3
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-01-24 12:26:17 +0530

    Update app_func.py

--- app_func.py
+++ app_func.py
@@ -792,7 +792,7 @@ def predict(data_raw):
     
     # with open("files/report.pdf", "rb") as pdf_file: 
     #     encoded_string = base64.b64encode(pdf_file.read()) 
-    data_pdf={'PDF':"http://localhost:8000/files/report.pdf"} 
+    data_pdf={'PDF':"https://scavaivisionapi.azurewebsites.net/files/report.pdf"} 
     # data_pdf['PDF']= [t.encode('utf-8') for t in title]
 
     for i,j in zip(data_keys,obj_lst): ##Final Object
